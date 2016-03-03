1c8cb085586c90614360e60bb7da22d0ebfb99bc
Battaile Fauber <battaile@gmail.com>
2016-03-03 11:11:26 -0500

    single barebones tracker on a blank page

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 class App extends React.Component {
-  render() {
-    return <ProgressTracker />
-  }
+	render() {
+		return <ProgressTracker  / >
+	}
 }
 
 class ProgressTracker extends React.Component {
-  render() {
-    return <p><input id="target" type="text" /></p>
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			actual: 0,
+			targetVal: 0
+		};
+	};
+	handleActualChange(event) {
+		this.setState({actual: event.target.value});
+	};
+	handleTargetChange(event) {
+		this.setState({targetVal: event.target.value});
+	};
+	render() {
+		return <p > < input type="text" value={this.state.actual} onChange={this.handleActualChange.bind(this)} / > 
+					< input type="text" value={this.state.targetVal} onChange={this.handleTargetChange.bind(this)} / > 
+		 {this.state.targetVal > 0 ? this.state.actual/this.state.targetVal : 0} < /p>
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render( < App / > , document.getElementById("app"));
